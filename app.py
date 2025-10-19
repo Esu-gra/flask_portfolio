985b876705037042aafefe9d212f7a074548a0dd
esu grappasonni <esugrappa@gmail.com>
2025-10-19 10:56:47 +0200

    Rimosso form contatti temporaneamente

--- app.py
+++ app.py
@@ -35,34 +35,34 @@ def skills():
     return render_template("skills.html")
 
 # === PAGINA CONTATTI ===
-@app.route("/contact", methods=["GET", "POST"])
-def contact():
-    if request.method == "POST":
-        nome = request.form.get("nome")
-        cognome=request.form.get("cognome")
-        email = request.form.get("email")
-        messaggio = request.form.get("messaggio")
+# @app.route("/contact", methods=["GET", "POST"])
+# def contact():
+#     if request.method == "POST":
+#         nome = request.form.get("nome")
+#         cognome=request.form.get("cognome")
+#         email = request.form.get("email")
+#         messaggio = request.form.get("messaggio")
 
-        if not nome or not email or not messaggio:
-            flash("Tutti i campi sono obbligatori.", "error")
-            return redirect(url_for("contact"))
+#         if not nome or not email or not messaggio:
+#             flash("Tutti i campi sono obbligatori.", "error")
+#             return redirect(url_for("contact"))
 
-        data = {"nome": nome,"cognome": cognome,"email": email, "messaggio": messaggio}
+#         data = {"nome": nome,"cognome": cognome,"email": email, "messaggio": messaggio}
 
-        if not os.path.exists("messages.json"):
-            with open("messages.json", "w", encoding="utf-8") as f:
-                json.dump([], f)
+#         if not os.path.exists("messages.json"):
+#             with open("messages.json", "w", encoding="utf-8") as f:
+#                 json.dump([], f)
 
-        with open("messages.json", "r+", encoding="utf-8") as f:
-            messaggi = json.load(f)
-            messaggi.append(data)
-            f.seek(0)
-            json.dump(messaggi, f, indent=4, ensure_ascii=False)
+#         with open("messages.json", "r+", encoding="utf-8") as f:
+#             messaggi = json.load(f)
+#             messaggi.append(data)
+#             f.seek(0)
+#             json.dump(messaggi, f, indent=4, ensure_ascii=False)
 
-        flash("Messaggio inviato con successo!", "success")
-        return redirect(url_for("contact"))
+#         flash("Messaggio inviato con successo!", "success")
+#         return redirect(url_for("contact"))
 
-    return render_template("contact.html")
+#     return render_template("contact.html")
 @app.route("/about")
 def about():
     return render_template("about.html")
